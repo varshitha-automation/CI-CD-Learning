29031d46a2508001b39b5bcb2797c1c73b199f20
varshitha-automation <varshitha.m29@gmail.com>
2023-08-03 17:02:40 +0530

    first commit

---
+++ src/test/java/docker/jenkins/dockerTest.java
@@ -0,0 +1,13 @@
+package docker.jenkins;
+
+import org.testng.annotations.Test;
+
+public class dockerTest extends base
+{
+    @Test
+    public void dockerCICD()
+    {
+    	init();
+        System.out.println("learning CI/CD");
+    }
+}
