aa117f25d43c24685785d9bb7893349f2cf47420
varshitha-automation <varshitha.m29@gmail.com>
2023-08-05 12:48:08 +0530

    actual code

--- src/test/java/docker/jenkins/dockerTest.java
+++ src/test/java/docker/jenkins/dockerTest.java
@@ -1,6 +1,7 @@
 package docker.jenkins;
 
 import org.openqa.selenium.By;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 public class dockerTest extends base
@@ -10,14 +11,15 @@ public class dockerTest extends base
     {
     	init();
         System.out.println("learning CI/CD");
+        driver.get("http://localhost:9091/webapp/");
+        Assert.assertTrue(driver.findElement(By.tagName("h2")).isDisplayed());
+        driver.close();
     }
     
-    @Test(priority=2)
-    public void learn()
-    {
-    	init();
-        driver.findElement(By.id("APjFqb")).sendKeys("hi");
-        System.out.println("learning first CI/CD");
-        System.out.println("===================");
-    }
+	/*
+	 * @Test(priority=2) public void learn() { init();
+	 * driver.findElement(By.id("APjFqb")).sendKeys("hi");
+	 * System.out.println("learning first CI/CD");
+	 * System.out.println("==================="); }
+	 */
 }
